1a806b07f99df576756299ce2f80e1deffcae39d
Edison Guaichico <guaichicoeison@gmail.com>
2023-07-13 20:27:14 +0200

    cambiando variables de entorno

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.PORT = process.env.PORT || 3003 
+
+exports.DB_HOST  = process.env.DB_HOST  || 'localhost' 
+exports.DB_USER  = process.env.DB_USER  || 'postgres' 
+exports.DB_PASSWORD  = process.env.DB_PASSWORD  || 'entraste' 
+exports.DB_NAME  = process.env.DB_NAME  || 'revisionConstruccion' 
+exports.DB_PORT  = process.env.DB_PORT  || '5432' 
